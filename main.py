4d80ce237d7fb67c6bbaf07cf3f1adfb7dc0d5ed
dorohovgeorge <dorohovgeorge@gmail.com>
2022-09-29 16:21:54 +0300

    LAB1 - done.

---
+++ main.py
@@ -0,0 +1,69 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense
+from keras import backend as k
+from keras_preprocessing.image import ImageDataGenerator
+
+
+# data augmentation
+def data_augmentation_generator(x, y, batch_size):
+    train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                       height_shift_range=.25, )
+    generator = train_datagen.flow(x, y, batch_size=batch_size)
+    while 1:
+        x_batch, y_batch = generator.next()
+        yield ([x_batch, y_batch])
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+img_rows, img_cols = 28, 28
+
+if k.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    inpx = (1, img_rows, img_cols)
+
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    inpx = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+
+batch_size = 150
+
+data_augmentation_generator(x_train, y_train, batch_size)
+input_shape = (28, 28, 1)
+num_classes = 10
+epochs = 10
+
+model = keras.models.Sequential([
+    keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu', input_shape=input_shape),
+    keras.layers.Conv2D(32, (5, 5), padding='same', activation='relu'),
+    keras.layers.MaxPool2D(),
+    keras.layers.Dropout(0.25),
+    keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+    keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu'),
+    keras.layers.MaxPool2D(strides=(2, 2)),
+    keras.layers.Dropout(0.25),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model.compile(optimizer=keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
+
+model.fit(x_train, y_train, epochs=epochs, batch_size=500)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('loss=', score[0])
+print('accuracy=', score[1])
